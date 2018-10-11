290d5719be6e69f1c7e749c8988950aff501d8e7
Linux <kmoukin@gmail.com>
2018-10-11 17:42:21 -0400

    AddFormContact

--- project/web/forms.py
+++ project/web/forms.py
@@ -7,3 +7,8 @@ class LoginForm(forms.Form):
 
     password = forms.CharField(max_length=100, widget=forms.PasswordInput)
 
+
+class ContactForm(forms.Form):
+    sujet = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    envoyeur = forms.EmailField(label="Votre adresse e-mail")
