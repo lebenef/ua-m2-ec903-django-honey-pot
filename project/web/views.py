1e39d36e8a9a337a1e8c57e40c2049d33214021d
Linux <kmoukin@gmail.com>
2018-10-11 12:55:34 -0400

    ModifView.py

--- project/web/views.py
+++ project/web/views.py
@@ -1,5 +1,24 @@
-from django.http import HttpResponse
+from .forms import ContactForm
 
 
-def index(request):
-    return HttpResponse("En Construction")
+def contact(request):
+
+
+    form = ContactForm(request.POST or None)
+
+
+    if form.is_valid(): 
+
+
+        username = form.cleaned_data['username']
+
+        passworr = form.cleaned_data['password']
+
+
+        envoi = True
+
+    
+
+    # Quoiqu'il arrive, on affiche la page du formulaire.
+
+    return render(request, 'view.html', locals())
