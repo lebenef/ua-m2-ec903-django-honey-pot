13d259f135b64afc878625b7b977b5962406d1d8
Linux <kmoukin@gmail.com>
2018-10-11 14:26:34 -0400

    DeleteForm.py

--- project/web/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class LonginForm(forms.Form):
-
-    username = forms.CharField(max_length=100)
-
-    password = forms.CharField(max_length=100, widget=forms.Passwordinput)
-
