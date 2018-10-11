6889a545f445ca23cca74a7a9d869046bb44a2e7
Linux <kmoukin@gmail.com>
2018-10-11 17:40:32 -0400

    AddUrls

--- project/web/urls.py
+++ project/web/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('contact/', views.contact, name='contact'),
+    path('condutil/', views.condutil, name="Condition d'utilisation"),
+    path('donneeperso/', views.donneeperso, name='Données Personelles'),
+    path('menleg/', views.menleg, name='Mention Legal'),
+
 ]
