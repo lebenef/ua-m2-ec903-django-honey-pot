4dc766d43264a197aada82088420e85568cf1419
Linux <kmoukin@gmail.com>
2018-10-11 18:35:11 -0400

    AddModels

--- project/web/models.py
+++ project/web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+
+
+class Contact(models.Model):
+    sujet = models.CharField(max_length=100)
+    message = models.TextField()
+    envoyeur = models.CharField(max_length=100)
+    
+    
+
+
+class Data(models.Model):
+    addressip = models.CharField(max_length=100)
+    useragent = models.CharField(max_length=100)
+    datetime = models.DateField()
+
+
