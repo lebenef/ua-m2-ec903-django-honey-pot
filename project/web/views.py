5350a3953bfc88550b974705a73b6bf69b5512b9
Linux <kmoukin@gmail.com>
2018-10-11 20:48:01 -0400

    ModifiViews

--- project/web/views.py
+++ project/web/views.py
@@ -1,28 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+#from .forms import UserForm
 from .forms import ContactForm
+from .forms import InscriptionForm
 
 def index(request):
     return HttpResponse("En construction")
 
+def inscription(request):
+
+
+    form = InscriptionForm(request.POST or None)
+
+
+    if form.is_valid():
+
+
+
+        form.save()
+
+
+    return render(request, 'web/inscription.html', locals())
+
 def login(request):
 
-
-    form = LoginForm(request.POST or None)
-
-
-    if form.is_valid(): 
-
-
-        username = form.cleaned_data['username']
-
-        password = form.cleaned_data['password']
-
-
-        envoi = True
-
-    
 
 
     return render(request, 'web/login.html', locals())
@@ -34,11 +35,9 @@ def contact(request):
     form = ContactForm(request.POST or None)
     
     if form.is_valid(): 
-        sujet = form.cleaned_data['sujet']
-        message = form.cleaned_data['message']
-        envoyeur = form.cleaned_data['envoyeur']
         
-        envoi = True
+        form.save()
+
     return render(request, 'web/contact.html', locals())
 
 def condutil(request):
@@ -51,5 +50,4 @@ def donneeperso(request):
     return render(request, 'web/donneeperso.html', locals())
 
 def menleg(request):
-
     return render(request, 'web/menleg.html', locals())
