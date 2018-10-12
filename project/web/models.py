825b07b8776908f825115aecca237d4db4c04d79
Linux <kmoukin@gmail.com>
2018-10-11 20:47:10 -0400

    ModifModel

--- project/web/models.py
+++ project/web/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
 
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     mail = models.CharField(max_length=100)
+
+    def __str__(self):
+      
+        return self.username
 
 
 class Contact(models.Model):
@@ -14,11 +18,22 @@ class Contact(models.Model):
     envoyeur = models.CharField(max_length=100)
     
     
+    def __str__(self):
+
+        return self.sujet
+
+    
+    
 
 
 class Data(models.Model):
     addressip = models.CharField(max_length=100)
     useragent = models.CharField(max_length=100)
     datetime = models.DateField()
+
+    def __str__(self):
+
+        return self.addressip
+
 
 
