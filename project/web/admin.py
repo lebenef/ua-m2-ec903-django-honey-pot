3d95e7eea87f14fc610b903ecaed750a81448657
Linux <kmoukin@gmail.com>
2018-10-11 20:46:46 -0400

    AddDatabaseInAdminPannel

--- project/web/admin.py
+++ project/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import User
+from .models import Contact
+from .models import Data
 # Register your models here.
+
+admin.site.register(User)
+
+admin.site.register(Contact)
+
+admin.site.register(Data)
